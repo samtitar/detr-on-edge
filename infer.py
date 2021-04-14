44ada2a88602c82fa2ad497391631207f812a57d
Sam Titarsolej <s.titarsolej@gmail.com>
2021-04-14 14:15:08 +0200

    Model setup for backbone inference testing

---
+++ infer.py
@@ -0,0 +1,87 @@
+from PIL import Image
+from models import DETRv1
+
+import torch
+import torchvision.transforms as T
+
+import time
+import requests
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use('TkAgg')
+
+detr = DETRv1(num_classes=91, backbone='resnet34')
+detr.eval()
+
+CLASSES = [
+    'N/A', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+    'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A',
+    'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse',
+    'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack',
+    'umbrella', 'N/A', 'N/A', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis',
+    'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove',
+    'skateboard', 'surfboard', 'tennis racket', 'bottle', 'N/A', 'wine glass',
+    'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich',
+    'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake',
+    'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table', 'N/A',
+    'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard',
+    'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A',
+    'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier',
+    'toothbrush'
+]
+
+COLORS = [[0.000, 0.447, 0.741], [0.850, 0.325, 0.098], [0.929, 0.694, 0.125],
+          [0.494, 0.184, 0.556], [0.466, 0.674, 0.188], [0.301, 0.745, 0.933]]
+
+transform = T.Compose([
+    T.Resize(800),
+    T.ToTensor(),
+    T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+def box_cxcywh_to_xyxy(x):
+    x_c, y_c, w, h = x.unbind(1)
+    b = [(x_c - 0.5 * w), (y_c - 0.5 * h),
+         (x_c + 0.5 * w), (y_c + 0.5 * h)]
+    return torch.stack(b, dim=1)
+
+def rescale_bboxes(out_bbox, size):
+    img_w, img_h = size
+    b = box_cxcywh_to_xyxy(out_bbox)
+    b = b * torch.tensor([img_w, img_h, img_w, img_h], dtype=torch.float32)
+    return b
+
+def detect(im, model, transform):
+    img = transform(im).unsqueeze(0)
+
+    assert img.shape[-2] <= 1600 and img.shape[-1] <= 1600, 'demo model only supports images up to 1600 pixels on each side'
+
+    outputs = model(img)
+    probas = outputs['classes'].softmax(-1)[0, :, :-1]
+    mask = probas.max(-1).values > 0.7
+
+    bboxes_scaled = rescale_bboxes(outputs['bboxes'][0, mask], im.size)
+    return probas[mask], bboxes_scaled
+
+url = 'http://images.cocodataset.org/val2017/000000039769.jpg'
+im = Image.open(requests.get(url, stream=True).raw)
+
+s_time = time.time()
+scores, boxes = detect(im, detr, transform)
+print(f'Inference time: {time.time() - s_time}')
+
+def plot_results(pil_img, prob, boxes):
+    plt.figure(figsize=(16,10))
+    plt.imshow(pil_img)
+    ax = plt.gca()
+    for p, (xmin, ymin, xmax, ymax), c in zip(prob, boxes.tolist(), COLORS * 100):
+        ax.add_patch(plt.Rectangle((xmin, ymin), xmax - xmin, ymax - ymin,
+                                   fill=False, color=c, linewidth=3))
+        cl = p.argmax()
+        text = f'{CLASSES[cl]}: {p[cl]:0.2f}'
+        ax.text(xmin, ymin, text, fontsize=15,
+                bbox=dict(facecolor='yellow', alpha=0.5))
+    plt.axis('off')
+    plt.show()
+    
+plot_results(im, scores, boxes)
---
+++ models.py
@@ -0,0 +1,57 @@
+import torchvision.models as backbones
+
+import torch
+import torch.nn as nn
+
+BACKBONES = ['resnet50', 'resnet34', 'resnet18']
+
+class DETRv1(nn.Module):
+    def __init__(self, num_classes, hidden_dim=256, nheads=8,
+                 num_encoder_layers=6, num_decoder_layers=6,
+                 backbone='resnet50'):
+        
+        assert backbone in BACKBONES, f'Invalid backbone {backbone}, select from: {BACKBONES}'
+
+        super().__init__()
+        self.backbone = getattr(backbones, backbone)()
+        del self.backbone.fc
+        
+        if backbone == 'resnet50':
+            self.conv = nn.Conv2d(2048, hidden_dim, 1)
+        elif backbone == 'resnet34' or backbone == 'resnet18':
+            self.conv = nn.Conv2d(512, hidden_dim, 1)
+
+        self.transformer = nn.Transformer(hidden_dim, nheads,
+                                          num_decoder_layers,
+                                          num_decoder_layers)
+        
+        self.fc_class = nn.Linear(hidden_dim, num_classes + 1)
+        self.fc_bbox = nn.Linear(hidden_dim, 4)
+
+        self.query_pos = nn.Parameter(torch.rand(100, hidden_dim))
+        self.row_embed = nn.Parameter(torch.rand(50, hidden_dim // 2))
+        self.col_embed = nn.Parameter(torch.rand(50, hidden_dim // 2))
+
+    def forward(self, x):
+        x = self.backbone.conv1(x)
+        x = self.backbone.bn1(x)
+        x = self.backbone.relu(x)
+        x = self.backbone.maxpool(x)
+
+        x = self.backbone.layer1(x)
+        x = self.backbone.layer2(x)
+        x = self.backbone.layer3(x)
+        x = self.backbone.layer4(x)
+
+        x = self.conv(x)
+        H, W = x.shape[-2:]
+        pos = torch.cat([
+            self.col_embed[:W].unsqueeze(0).repeat(H, 1, 1),
+            self.row_embed[:H].unsqueeze(1).repeat(1, W, 1),
+        ], dim=-1).flatten(0, 1).unsqueeze(1)
+
+        x = self.transformer(pos + 0.1 * x.flatten(2).permute(2, 0, 1),
+                             self.query_pos.unsqueeze(1)).transpose(0, 1)
+        
+        return {'classes': self.fc_class(x),
+                'bboxes': self.fc_bbox(x).sigmoid()}
