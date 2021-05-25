fb28616be95b3f234a93c45624bbc91a4852cd04
Sam Titarsolej <s.titarsolej@gmail.com>
2021-05-25 17:25:50 +0200

    refactor & cleanup

--- torch2onnx.py
+++ torch2onnx.py
@@ -1,15 +1,9 @@
 from PIL import Image
+from numpy import interp
 from models import DETRv1, ResnetWrap, InterpreterWrap
 
 import torch
-import torchvision
-import torchvision.transforms as T
-
-import os
-import onnx
-import requests
 import argparse
-import numpy as np
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -18,11 +12,11 @@ if __name__ == '__main__':
     parser.add_argument('--backbone', type=str, default='resnet50')
     args = parser.parse_args()
 
-    in_tensor = torch.zeros((1, 3, 512, 512))
+    in_tensor = torch.zeros((1, 3, 224, 224))
     if args.split == False:
         model = DETRv1(91, backbone=args.backbone).eval()
         
-        dynamic_axes = {'image': {0: 'batch', 2: 'width', 3: 'height'},
+        dynamic_axes = {'image': {0: 'batch'},
                         'class': {0: 'batch'}, 'bbox': {0: 'batch'}}
 
         torch.onnx.export(model, in_tensor, f'{args.outdir}/detr.onnx',
@@ -33,17 +27,18 @@ if __name__ == '__main__':
         backbone = ResnetWrap(args.backbone).eval()
         interpreter = InterpreterWrap(91).eval()
 
-        dynamic_axes = {'image': {0: 'batch', 2: 'width', 3: 'height'},
-                        'vector': {0: 'batch', 2: 'width', 3: 'height'}}
+        dynamic_axes = {'image': {0: 'batch'},
+                        'vector': {0: 'batch'}}
 
         torch.onnx.export(backbone, in_tensor, f'{args.outdir}/backbone.onnx',
                           input_names=['image'], output_names=['vector'],
                           dynamic_axes=dynamic_axes, opset_version=10)
-        in_tensor = backbone(in_tensor)
+        in_tensor = backbone(in_tensor) # Dims depend on resnet model
 
-        dynamic_axes = {'vector': {0: 'batch', 2: 'width', 3: 'height'},
+        dynamic_axes = {'vector': {0: 'batch'},
                         'class': {0: 'batch'}, 'bbox': {0: 'batch'}}
 
         torch.onnx.export(interpreter, in_tensor, f'{args.outdir}/interpreter.onnx',
                           input_names=['vector'], output_names=['class', 'bbox'],
                           dynamic_axes=dynamic_axes, opset_version=10)
+        a = interpreter(in_tensor)
